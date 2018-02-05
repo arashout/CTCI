616b5974514984d8b0fbd064137189b997a1e3ad
Arash Outadi <arash.out@gmail.com>
2018-02-05 22:07:41 +0000

    2 more down

---
+++ ctci/problems/13.py
@@ -0,0 +1,38 @@
+from Testing import Solution, Test
+from typing import List
+
+
+def urlify(char_list: List[str],  true_length: int) -> List[str]:
+    count_all_spaces = 0
+    for c in char_list:
+        if c == ' ':
+            count_all_spaces += 1
+
+    # Space is normally 1 character while %20 is 3
+    # 2 additional characters per space
+    count_spaces = count_all_spaces // 3
+    offset = count_spaces * 2
+    # Reverse for loop to take advantage of buffer at end
+    # and not overwriting anything when moving
+    for i in range((true_length - 1) - offset, 0, -1):
+        c = char_list[i]
+        if c != ' ':
+            char_list[i + offset] = c
+        else:
+            char_list[i + offset] = '0'
+            char_list[i + offset - 1] = '2'
+            char_list[i + offset - 2] = '%'
+            # For the buffer we have 'lost'
+            offset -= 2
+            if offset == 0:
+                return char_list
+    
+    # For testing
+    return char_list
+
+Solution(
+    urlify,
+    [
+        Test([list('Mr John Smith    '), 17], list('Mr%20John%20Smith')),
+    ]
+)
---
+++ ctci/problems/14.py
@@ -0,0 +1,57 @@
+from Testing import Test, Solution
+from typing import List
+
+def is_permutation_of_palindrome(char_list: List[str]) -> bool:
+    freq_table = {}
+
+    for c in char_list:
+        if c in freq_table:
+            freq_table[c] += 1
+        else:
+            freq_table[c] = 1
+    
+    count_odd = 0
+    for count in freq_table.values():
+        if count % 2 != 0 :
+            count_odd += 1
+            if count_odd > 1:
+                return False
+    
+    return True
+
+# Improved solution with bit vector
+def is_permutation_of_palindrome_optimized(char_list: List[str]) -> bool:
+    def toggle_bit(vector, k) -> int:
+        mask = 1 << k
+        return vector ^ mask
+    
+    # Python 32 bit integer so assuming only lowercase alphabet
+    bit_vector = 0
+    
+    offset = ord('a')
+    for c in char_list:
+        value = ord(c) - offset
+        bit_vector = toggle_bit(bit_vector, value)
+    
+    odd_mask = bit_vector - 1
+    return bit_vector == 0 or not (bit_vector & odd_mask)
+
+Solution(
+    is_permutation_of_palindrome,
+    [
+        Test([list('yaya')], True),
+        Test([list('yayaa')], True),
+        Test([list('baabcccddd')], False),
+        Test([list('')], True),
+    ]
+)
+
+Solution(
+    is_permutation_of_palindrome_optimized,
+    [
+        Test([list('yaya')], True),
+        Test([list('yayaa')], True),
+        Test([list('baabcccddd')], False),
+        Test([list('')], True),
+    ]
+)
