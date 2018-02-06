3d76d5749616abb4bb1a2015101a8b1264733f54
Arash Outadi <arash.outadi@ravelin.com>
2018-02-06 23:11:33 +0000

    Solved it by myself this time

---
+++ ctci/problems/16.py
@@ -0,0 +1,42 @@
+from typing import List
+from Testing import Solution, Test
+
+def compress_string(s: str) -> str:
+    new_s = ''
+    i = 0
+
+    while True:
+        count = 1
+        j = i + 1
+        while j < len(s):
+            if s[i] == s[j]:
+                count += 1
+                j += 1
+            else:
+                new_s += s[i] + str(count)
+                i = j
+                break
+
+        if j >= len(s):
+            if j > len(s):
+                return s
+            
+            new_s += s[i] + str(count)
+            break
+    
+    if len(new_s) >= len(s):
+        return s
+
+    return new_s
+
+Solution(
+    compress_string,
+    [
+        Test(['aaabb'], 'a3b2'),
+        Test(['aaabbba'], 'a3b3a1'),
+        Test(['a'], 'a'),
+        Test([''], ''),
+        Test(['aba'], 'aba'),
+        Test(['aaaaa'], 'a5'),
+    ]
+)
