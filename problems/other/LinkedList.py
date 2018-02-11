4c92219218211243d83d93975852cbe476a78466
Arash Outadi <arash.outadi@ravelin.com>
2018-02-11 22:10:58 +0000

    Linked list stufF

--- problems/other/LinkedList.py
+++ problems/other/LinkedList.py
@@ -1,13 +1,17 @@
+from Testing import Solution, Test
 from typing import NamedTuple, Any
+from abc import ABC
+
 
 class Node(NamedTuple):
     data: Any
 
     def __repr__(self):
         return str(self.data)
-    
+
     def __str__(self):
         return str(self.data)
+
 
 class SinglyNode(Node):
     next: Node
@@ -17,22 +21,77 @@ class SinglyNode(Node):
         self.next = None
         return self
 
+
 class DoublyNode(Node):
     next: Node
     prev: Node
 
-class SinglyLinkedList():
+
+class LinkedList(ABC):
+    def __init__(self, head: Node):
+        self.head = head
+
+
+class SinglyLinkedList(LinkedList):
     head: SinglyNode
 
     def __init__(self, head: SinglyNode):
         self.head = head
-    
+
     def __repr__(self):
         string = ''
         node = self.head
-        
+
         while node is not None:
             string += str(node) + ' > '
             node = node.next
 
         return string
+
+    def prepend(self, data: Any):
+        if self.h ead is None:
+            self.head = SinglyNode(data)
+        else:
+            temp = self.head
+            self.head = SinglyNode(data)
+            self.head.next = temp
+
+    def pop_front(self) -> Any:
+        if self.head is None:
+            # Maybe should raise error?
+            return None
+        data = self.head.data
+        self.head = self.head.next
+        return data
+
+    def reverse(self):
+        if self.head is None or self.head.next is None:
+            return self.head
+
+        prev = None
+        cur = self.head
+
+        while cur is not None:
+            temp = cur.next
+            cur.next = prev
+
+            prev = cur
+            cur = temp
+
+        return
+
+
+def parse(string: str, is_doubly_linked: bool = False) -> LinkedList:
+    if is_doubly_linked:
+        raise NotImplementedError
+    else:
+        sll = SinglyLinkedList(None)
+        split_string = string.replace(' ', '').split('>')
+        split_string = list(filter(None, split_string))
+
+        # Assume they are ints ->
+        # Could handle generics by having a parse_type method for each type
+        for i in range(len(split_string) - 1, -1, -1):
+            data = int(split_string[i])
+            sll.prepend(data)
+        return sll
--- problems/other/mergeSortedLists.py
+++ problems/other/mergeSortedLists.py
@@ -1,12 +1,13 @@
 from Testing import Solution, Test
-from LinkedList import SinglyLinkedList, SinglyNode
+from LinkedList import SinglyLinkedList, SinglyNode, parse
 
-def mergeSortedLists(a: list, b: list) -> list:
+
+def merge_sorted_lists(a: list, b: list) -> list:
     n = len(a)
     m = len(b)
-    
+
     # Pre-allocate list
-    c = [None] * (m+n)
+    c = [None] * (m + n)
 
     i = 0
     j = 0
@@ -29,15 +30,45 @@ def mergeSortedLists(a: list, b: list) -> list:
         k += 1
     return c
 
+
 Solution(
-    mergeSortedLists,
+    merge_sorted_lists,
     [
-        Test([ [1,3,5], [1, 2, 5, 7] ], [1, 1, 2, 3, 5, 5, 7]),
-        Test([ [], [1, 2, 5] ], [1, 2, 5]),
+        Test([[1, 3, 5], [1, 2, 5, 7]], [1, 1, 2, 3, 5, 5, 7]),
+        Test([[], [1, 2, 5]], [1, 2, 5]),
     ]
 )
 
-def mergeSortedLinkedLists(a: SinglyLinkedList):
-    print(a)
 
-mergeSortedLinkedLists(SinglyLinkedList(SinglyNode(4)))
+def merge_sorted_linked_lists(a: SinglyLinkedList, b: SinglyLinkedList) -> SinglyLinkedList:
+    def rec(i: SinglyNode, j: SinglyNode, k: SinglyNode):
+        if i is None and j is None:
+            return
+        else:
+            if i is None:
+                k.next = j
+            elif j is None:
+                k.next = i
+            else:
+                if i.data < j.data:
+                    k.next = SinglyNode(i.data)
+                    rec(i.next, j, k.next)
+                else:
+                    k.next = SinglyNode(j.data)
+                    rec(i, j.next, k.next)
+
+    c = SinglyLinkedList(SinglyNode(None))
+    rec(a.head, b.head, c.head)
+    c.pop_front()
+    return c
+        
+def merge_test_helper(a: SinglyLinkedList, b: SinglyLinkedList) -> str:
+    return str(merge_sorted_linked_lists(a, b))
+
+Solution(
+    merge_test_helper,
+    [
+        Test([parse('1 > 3 > 5'), parse('1 > 5 > 7')], '1 > 1 > 3 > 5 > 5 > 7 > '),
+        Test([parse('1 > 1'), parse('')], '1 > 1 > ')
+    ]
+)
